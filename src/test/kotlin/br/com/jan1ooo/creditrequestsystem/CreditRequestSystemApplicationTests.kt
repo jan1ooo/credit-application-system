aa2a8936ab82e47b0a93287a41732572e15bd28a
Janio Filho <filhojanio67@gmail.com>
2023-09-08 23:38:13 -0300

    First commit

---
+++ src/test/kotlin/br/com/jan1ooo/creditrequestsystem/CreditRequestSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jan1ooo.creditrequestsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditRequestSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
